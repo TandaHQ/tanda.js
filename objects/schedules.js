a14303668385517cb9b3221909f088edd3faa06e
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-05-04 19:24:51 +1000

    implement rudimentary schedules api. full docs

--- objects/schedules.js
+++ objects/schedules.js
@@ -1,5 +1,57 @@
 var Promise = require('bluebird');
 
 module.exports = function(){
+    let endpoint = '/schedules';
+    
+    var methods = {};
+    
+    /**
+     * Get all schedules in the provided list
+     * @param {array} ids - List of ids to query
+     * @param {boolean} show_costs - Show costs?
+     */
+    methods.gets = (ids, show_costs) => {
+        var url = endpoint + '?ids=' + ids.join(',') + this._.show(show_costs, false);
+        return this.request('GET', url);
+    };
 
+    /**
+     * Create a schedule
+     * @param {object} schedule - the object to create
+     */
+    methods.create = (schedule) => {
+        return this.request('POST', endpoint, schedule);
+    };
+
+    /**
+     * Get a schedule by schedule id
+     * @param {int} id - Schedule ID to query on
+     * @param {boolean} show_costs - Show Costs?
+     * @returns {Promise} Resolve: the schedule.  Reject: {err: 'The error'}
+     */
+    methods.get = (id, show_costs) => {
+        var url = endpoint + `/${id}` + this._.show(show_costs);
+        return this.request('GET', url);
+    };
+
+    /**
+     * Update a schedule, identified by a Schedule ID
+     * @param {int} id - Schedule ID to update
+     * @param {object} update - Data to update with
+     * @returns {Promise} Resolve: the updated schedule. Reject: {err : 'The error'}
+     */
+    methods.update = (id, update) => {
+        return this.request('PUT', `${endpoint}/${id}`, update);
+    };
+
+    /**
+     * Delete a schedule
+     * @param {int} id - Schedule ID to delete
+     * @returns {Promise} Resolve: nothing (status 204 is success). Reject: {err : 'The error}
+     */
+    methods.delete = (id) => {
+        return this.request('DELETE', `${endpoint}/${id}`);
+    };
+    
+    return methods;
 };
