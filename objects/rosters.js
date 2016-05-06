e34ea2631deb82c354b8cea8f81af2e9e7a156b0
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-05-06 18:22:00 +1000

    move to lighter version

--- objects/rosters.js
+++ objects/rosters.js
@@ -3,16 +3,8 @@ var Promise = require('bluebird');
 module.exports = function(){
   let endpoint = 'rosters/';
   var get = (id, show_costs) => {
-    return new Promise((resolve, reject) => {
-      var url = endpoint + id + this._.show(show_costs);
-      this.request('GET', url)
-        .then(function(body){
-          return resolve(body);
-        })
-        .catch(function(err){
-          return reject(err);
-        });
-    });
+    var url = endpoint + id + this._.show(show_costs);
+    return this.request('GET', url);
   };
 
   var current = (show_costs) => {
