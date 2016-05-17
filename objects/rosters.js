f70818ba1662a21f8c22fe259eaa09471b75682e
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-05-17 18:26:29 +1000

    change the last roster endpoint to a better method

--- objects/rosters.js
+++ objects/rosters.js
@@ -3,19 +3,11 @@ var Promise = require('bluebird');
 module.exports = function(){
   let endpoint = 'rosters/';
   var get = (id, show_costs) => {
-    return new Promise((resolve, reject) => {
-      var url = endpoint + id;
-      if (show_costs){
-        url += '?show_costs=true';
-      }
-      this.request('GET', url)
-        .then(function(body){
-          return resolve(body);
-        })
-        .catch(function(err){
-          return reject(err);
-        });
-    });
+    var url = endpoint + id;
+    if (show_costs){
+      url += '?show_costs=true';
+    }
+    return this.request('GET', url);
   };
 
   var current = (show_costs) => {
