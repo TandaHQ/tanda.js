018bb232035392b13b7c8cc32831c2d2c938a2e3
David Buchan-Swanson <david.buchanswanson@gmail.com>
2016-11-13 15:52:25 +1000

    add a generic endpoint getter

--- src/endpoints/Endpoint.js
+++ src/endpoints/Endpoint.js
@@ -5,7 +5,17 @@ export default class Endpoint {
     this.Tanda = parent;
   }
 
-  request(...args) {
-    this.Tanda.request(...args);
+  get endpoint() {
+    return `/${this.constructor.name.toLowerCase()}`;
+  }
+
+  /**
+   * Localised request method
+   * @param {String} endpoint The enpoint to post to
+   * @param {String} method The HTTP method [GET, PUT, POST, DELETE]
+   * @param {Array|Object} data The data to send.  For a get request, will be turned into URL params
+   */
+  request(endpoint, method, data) {
+    return this.Tanda.request(endpoint, method, data);
   }
 }
